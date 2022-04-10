0bbe633d481f716916aac351b3488d0ca0f77e43
radheshyam patil <raeheshyampatil8503@gmail.com>
2022-04-10 14:48:44 +0530

    minor bugs

--- Backend/data/products.js
+++ Backend/data/products.js
@@ -42,7 +42,7 @@ const products = [
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     brand: 'Sony',
     category: 'Electronics',
-    price: 4999.00,
+    price: 5999.00,
     countInStock: 11,
     rating: 5,
     numReviews: 12,
